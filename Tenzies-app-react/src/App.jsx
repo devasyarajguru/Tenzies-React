bc389005ce6f25883b4310d92951e07439af3db1
Devasya <devasyarajguru@gmail.com>
2024-01-09 17:55:22 +0530

    feat:App Component and it's css to load other components, elements

---
+++ Tenzies-app-react/src/App.jsx
@@ -0,0 +1,10 @@
+// import React from 'react'; // In newer versions of React(since React 17) it is not necessary to import React in files where JSX is used
+import './app.css'
+export default function App()
+{
+  return(
+    <>
+      <main>Some</main>
+    </>
+  )
+}
