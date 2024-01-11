c97400e2072444f6ed557899f77e264a81237967
Devasya <devasyarajguru@gmail.com>
2024-01-11 12:29:46 +0530

    Setting the random numbers which are generated by the dice roll button to a state value

--- Tenzies-app-react/src/App.jsx
+++ Tenzies-app-react/src/App.jsx
@@ -1,14 +1,19 @@
 // import React from 'react'; // In newer versions of React(since React 17) it is not necessary to import React in files where JSX is used
 import './app.css'
 import Dice from './Components/Dice'
+import { useState } from 'react';
 
 
 export default function App()
 {
+  const[diceValues,setdiceValues] = useState(allNewDice())
+
+  console.log(diceValues)
+
   function allNewDice()
   {
     const result = [];
-    for(let i=0;i<6;i++)
+    for(let i=0;i<10;i++)
     {
       const randomNum = Math.ceil(Math.random() * 6);
       result.push(randomNum)
@@ -16,25 +21,25 @@ export default function App()
       return result
   }
 
-  const randomNumbers = allNewDice();
-  console.log(randomNumbers)
+  function rollDice()
+  {
+    setdiceValues(allNewDice())
+  }
+    
+  // const randomNumbers = allNewDice();
+  // console.log(randomNumbers)
+
+  const diceElements = diceValues.map((value,index) => <Dice value={value} key={index}/>)
+
   return(
     <>
     {/* Main element for which our Dice component will render */}
       <main>
         {/* Dice Container of our Dice Component */}
         <div className='dice-container'>
-        <Dice value="1"/>
-        <Dice value="1"/>
-        <Dice value="1"/>
-        <Dice value="1"/>
-        <Dice value="1"/>
-        <Dice value="1"/>
-        <Dice value="1"/>
-        <Dice value="1"/>
-        <Dice value="1"/>
-        <Dice value="1"/>
+          {diceElements}
         </div>
+        <button onClick={rollDice}>Roll</button>
       </main>
     </>
   )
